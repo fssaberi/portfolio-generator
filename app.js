4d05116c63e53cd1f533bafbca769a03223205bc
Fatemeh Brizendine <ssaberi324@yahoo.com>
2021-12-05 18:36:22 -0800

    add validation

--- app.js
+++ app.js
@@ -15,12 +15,28 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username'
+      message: 'Enter your GitHub Username',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your Github Username!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -49,7 +65,15 @@ const promptProject = portfolioData => {
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please provide a description of your project!');
+          return false;
+        }
+      }
     },
     {
       type: 'checkbox',
@@ -61,6 +85,14 @@ const promptProject = portfolioData => {
       type: 'input',
       name: 'link',
       message: 'Enter the GitHub link to your project. (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your Project Github link!');
+          return false;
+        }
+      }
     },
     {
       type: 'confirm',
